db31a96d23cfb65c04e06ce3eeac7077b365c261
Maria Yasmin <mariayasminfer@gmail.com>
2025-12-21 17:49:57 -0300

    Getter e Setter criados na classe curso

--- src/main/java/com/example/gestao_academica/model/Curso.java
+++ src/main/java/com/example/gestao_academica/model/Curso.java
@@ -1,8 +1,12 @@
 package com.example.gestao_academica.model;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Curso")
+@Getter
+@Setter
 public class Curso {
     
 
@@ -18,41 +22,5 @@ public class Curso {
 
     private String descricao;
 
-    public Curso(){
-
-    }
-       
-        public Long getId(){
-             return id;
-        }
-
-        public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSigla(){
-        return sigla;
-    }
-
-    public void setSigla(String sigla) {
-        this.sigla = sigla;
-    }
-
-    public String getDescricao(){
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    
+   
 }
