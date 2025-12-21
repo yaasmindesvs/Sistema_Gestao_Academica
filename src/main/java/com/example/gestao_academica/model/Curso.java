7657b4c4e69e147213b38d320dcf472ccd0bf782
Maria Yasmin <mariayasminfer@gmail.com>
2025-12-21 16:53:44 -0300

    Classe curso feita

--- src/main/java/com/example/gestao_academica/model/Curso.java
+++ src/main/java/com/example/gestao_academica/model/Curso.java
@@ -1,5 +1,58 @@
 package com.example.gestao_academica.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "Curso")
 public class Curso {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String sigla;
+
+    private String descricao;
+
+    public Curso(){
+
+    }
+       
+        public Long getId(){
+             return id;
+        }
+
+        public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla(){
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     
 }
