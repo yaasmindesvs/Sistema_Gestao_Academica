e6be3e691719cc25020a5da3fe11b35e0051e04e
Maria Yasmin <mariayasminfer@gmail.com>
2025-12-21 17:55:11 -0300

    Criação do Getter e Setter na classe aluno

--- src/main/java/com/example/gestao_academica/model/Aluno.java
+++ src/main/java/com/example/gestao_academica/model/Aluno.java
@@ -1,5 +1,12 @@
 package com.example.gestao_academica.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "Aluno")
+@Getter
+@Setter
 public class Aluno {
     
 }
