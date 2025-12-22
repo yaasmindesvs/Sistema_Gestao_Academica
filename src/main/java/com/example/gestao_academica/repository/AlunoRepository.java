d1507022a26d3605159af19aa536dee143af36df
Maria Yasmin <mariayasminfer@gmail.com>
2025-12-21 23:16:02 -0300

    Repositorio aluno criado

---
+++ src/main/java/com/example/gestao_academica/repository/AlunoRepository.java
@@ -0,0 +1,10 @@
+package com.example.gestao_academica.repository;
+
+import com.example.gestao_academica.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+
+
+    boolean existsByCursoId(Long cursoId);
+} 
